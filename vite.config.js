27a883e64345f486310f47645f12db3eec36a438
Nish1234Nish <nishaanthrangan8013@gmail.com>
2025-12-04 20:29:09 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
+export default {
   build: {
     outDir: 'dist',
-    sourcemap: false
+    rollupOptions: {
+      input: './index.html'
+    }
+  },
+  esbuild: {
+    loader: 'jsx',
+    include: /src\/.*\.jsx?$/,
+    exclude: []
+  },
+  optimizeDeps: {
+    esbuild: {
+      loader: {
+        '.js': 'jsx',
+        '.jsx': 'jsx'
+      }
+    }
   }
-})
+}
